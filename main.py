52ca8a501fd12e3ae1a23500c3cc54affe98a6a2
Brookelyn Parslow <broookelyn@gmail.com>
2020-12-07 02:15:35 +0000

    write function for variable decoding, start FY2009 xml

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
- 
+import csv
+
+file = open('variable_key.csv')
+data = list(csv.reader(file))
+#split list of versions
+for line in data:
+  line[11] = line[11].split(';')
+
+# variable_name,description,scope,location_code,form,part,data_type,required,cardinality,rdb_table,xpath,version,production_rule,last_version_modified
+
+def GetFull(var_name, version):
+  for line in data: 
+    if (var_name == line[0]) & (version in line[11]):
+      return line[1], line[3]
+
+print(GetFull('Gaming', '2009v1.7')) 
